6b74b82e4a510da2f30bf9fedbc66e0bc1bbe994
Henry Zarza <henryacz7@gmail.com>
2020-09-19 21:02:21 -0500

    General fixes

--- src/screens/HowItWorks/i18n.js
+++ src/screens/HowItWorks/i18n.js
@@ -2,5 +2,8 @@ import i18next from 'i18next';
 
 i18next.addResources('en', 'HOW_IT_WORKS', {
   TITLE: 'How It Works',
-  NAV: 'How It Works'
+  NAV: 'How It Works',
+  MAIN_TEXT_1:
+    'Rule of Thumb is a crowd sourced court of public opinion where anyone and everyone can speak out and speak freely.',
+  MAIN_TEXT_2: "It's easy: You share your opinion, we analyse and put the data in a public report."
 });
--- src/screens/PastTrials/index.js
+++ src/screens/PastTrials/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import i18next from 'i18next';
 
+import styles from './styles.module.scss';
+
 function PastTrials() {
   return (
-    <div>
-      <h1 className='subtitle m-bottom-4'>{i18next.t('PAST_TRIALS:TITLE')}</h1>
-    </div>
+    <section className={styles.section}>
+      <h1 className='medium-title m-bottom-4'>{i18next.t('PAST_TRIALS:TITLE')}</h1>
+      <p className='base-text'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta tempore neque quasi incidunt,
+        nemo quidem exercitationem saepe reiciendis aliquam totam commodi quos et dolorum quia natus
+        illum assumenda ad aut.
+      </p>
+    </section>
   );
 }
 
